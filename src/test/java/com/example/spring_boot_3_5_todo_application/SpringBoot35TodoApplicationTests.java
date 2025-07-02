bf059fd8e582ee871b737bd912943ff4d6d5c007
Szymon Mozol <szymonmozol123@gmail.com>
2025-07-02 19:49:37 +0200

    First Commit

---
+++ src/test/java/com/example/spring_boot_3_5_todo_application/SpringBoot35TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_3_5_todo_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot35TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
