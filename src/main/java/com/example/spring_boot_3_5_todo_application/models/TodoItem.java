f141836cfe46b1742dd07d514dc7cffe781136d4
Szymon Mozol <szymonmozol123@gmail.com>
2025-07-04 14:10:44 +0200

    TodoItem, TodoItemRepo

---
+++ src/main/java/com/example/spring_boot_3_5_todo_application/models/TodoItem.java
@@ -0,0 +1,32 @@
+package com.example.spring_boot_3_5_todo_application.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "todo-items")
+public class TodoItem implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String description;
+    private Boolean isCompleted;
+
+    private Instant createdAt;
+    private Instant updatedAt;
+
+
+    @Override
+    public String toString(){
+        return String.format("TodoItem{id=%d, description = '%s', isCompleted ='%s', createdAt = '%s', updatedAt = '%s'}",
+                id, description, isCompleted, createdAt, updatedAt);
+    }
+
+}
---
+++ src/main/java/com/example/spring_boot_3_5_todo_application/resources/TodoItemRepository.java
@@ -0,0 +1,4 @@
+package com.example.spring_boot_3_5_todo_application.resources;
+
+public interface TodoItemRepository {
+}
