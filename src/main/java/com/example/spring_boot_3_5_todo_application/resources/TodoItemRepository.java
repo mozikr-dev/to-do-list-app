bad1308b80d309f1bc5a08a5ba87c64efa9cd742
Szymon Mozol <szymonmozol123@gmail.com>
2025-07-09 02:28:29 +0200

    update

--- src/main/java/com/example/spring_boot_3_5_todo_application/resources/TodoItemRepository.java
+++ src/main/java/com/example/spring_boot_3_5_todo_application/resources/TodoItemRepository.java
@@ -1,4 +1,9 @@
 package com.example.spring_boot_3_5_todo_application.resources;
 
-public interface TodoItemRepository {
+import com.example.spring_boot_3_5_todo_application.models.TodoItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
 }
---
+++ src/main/java/com/example/spring_boot_3_5_todo_application/services/TodoItemService.java
@@ -0,0 +1,12 @@
+package com.example.spring_boot_3_5_todo_application.services;
+
+import com.example.spring_boot_3_5_todo_application.resources.TodoItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoItemService {
+
+    @Autowired
+    private TodoItemRepository todoItemRepository;
+}
